35203f282dbe424245b3a0d79434bbf867a2aa51
yokodake <ngyj@cinnabar.fr>
2020-05-18 01:10:10 +0200

    implement GtcEnv

---
+++ src/Gtc.hs
@@ -0,0 +1,33 @@
+module Gtc where
+
+import System.FilePath
+
+-- | eval environment/context and other stuff
+data GtcEnv =
+  GtcEnv { flags :: Flags
+         -- ^ settings & cmdline passed flags
+         , target :: Target
+         -- ^ the file to be interpreted
+         } deriving Show
+
+
+-- | represents what has to be interpreted
+data Target = Target { name :: String
+                     -- ^ filename
+                     , path :: FilePath
+                     -- ^ path to the file
+                     } deriving (Eq, Show)
+
+-- | build the target from an absolute path
+mkTarget :: FilePath -> Target
+mkTarget p = Target{name=takeFileName p, path=takeDirectory p}
+
+-- | temporary
+type Flags = [Flag]
+
+-- | temporary
+type Flag = String
+
+parseFlag :: String -> Flag
+parseFlag ('-':s) = s
+parseFlag s       = s
--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,55 @@
+{-# language NamedFieldPuns #-}
 module Main where
 
-import Parser (parseExpr, parseTokens)
+import System.Environment
+
 import Control.Monad.Trans
 import System.Console.Haskeline
 
+import Parser (parseProgram, parseTokens)
+import Gtc
+
+-- @Options@ is for later,
+-- including bringing all definitions into scope of the repl
+repl :: GtcEnv -> IO ()
+repl _ = runInputT defaultSettings loop
+  where
+    prompt = " > "
+    loop = do
+      minput <- getInputLine prompt
+      case minput of
+        Nothing -> outputStrLn "Leaving."
+        Just input -> (liftIO $ process input) >> loop
+
+eval :: GtcEnv -> IO ()
+eval env = print env
+
+main :: IO ()
+main = do args <- getArgs
+          case args of
+            []        -> error "greentxt: no input file"
+            "repl":xs -> mkEnv (parseArgs xs) >>= repl
+            _         -> mkEnv (parseArgs args) >>= eval
+
+-- | repl input process
 process :: String -> IO ()
 process input = do
   let tokens = parseTokens input
   putStrLn ("Tokens: " ++ show tokens)
-  let ast = parseExpr input
+  let ast = parseProgram input
   putStrLn ("Syntax: " ++ show ast)
 
-main :: IO ()
-main = runInputT defaultSettings loop
+-- | build the GtcEnv
+mkEnv :: (Maybe FilePath, Flags) -> IO GtcEnv
+mkEnv (Nothing, fs) =
+  return GtcEnv{flags=fs, target=Target{name="",path=""}}
+mkEnv (Just fn, fs) =
+  do -- pwd <- getEnv "PWD"
+     return GtcEnv{flags=fs, target=mkTarget fn}
+
+-- | !TODO handle case where multiple files are given
+parseArgs :: [String] -> (Maybe String, Flags)
+parseArgs = foldl f (Nothing, [])
   where
-    loop = do
-      minput <- getInputLine "> "
-      case minput of
-        Nothing -> outputStrLn "Goodbye."
-        Just input -> (liftIO $ process input) >> loop
+    f (fn, fs) ('-':s) = (fn ,parseFlag s : fs)
+    f (_,  fs) s  = (Just s, fs)
